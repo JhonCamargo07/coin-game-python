2ae7537fd24a52a253b360b3c4ccff34bc854014
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-10-22 08:11:12 -0500

    Se creo el menu para el usuario

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+games_played = 0
 
 
-# Press the green button in the gutter to run the script.
+def game_start():
+    try:
+        print_welcome()
+        get_option()
+    except Exception as e:
+        print(f'Ocurri\u00f3 un error: {e}')
+
+
+def print_welcome():
+    welcome = 'Bienvenido a cara o sello'
+    print(f' {welcome} '.center(len(welcome) + 50, '='))
+    print(f'\u00bfCrees poder ganarle al destino? ... Prob\u00e9moslo')
+
+
+def get_option():
+    print()
+    print('Elige tu primero \u00bfcara o sello?')
+    option_user = input('Escribe "cara" o "sello": ').upper()
+    if option_user != 'CARA' and option_user != 'SELLO':
+        print('Cometiste un error, intentalo nuevamente...')
+        return get_option()
+    print('\u00bfBuena elecci\u00f3n? Pronto lo sabremos')
+    get_value_to_bet()
+
+
+def get_value_to_bet():
+    print()
+    value_to_bet = input('Ingresa el valor a apostar: ')
+
+    if not value_to_bet.isnumeric() or int(value_to_bet) <= 0:
+        print('Valor incorrecto, por favor...')
+        return get_value_to_bet()
+    return int(value_to_bet)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    game_start()
